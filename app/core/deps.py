d7994f1d1d1f3672f0b0b656726dbd516334cb4e
huynhloc04 <hloc5874@gmail.com>
2024-08-05 20:28:44 +0700

    feat: security_bearer

--- app/core/deps.py
+++ app/core/deps.py
@@ -1,24 +1,28 @@
 from datetime import datetime
-from fastapi import Depends, HTTPException, status
+from fastapi import Security, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 from jose import jwt
 from pydantic import ValidationError
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
 
 from app.core.config import settings
 from app.db.session import get_session
 from app.repository.base import BaseRepository
 from app.models.user import User, TokenPayload
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/users/login", scheme_name="JWT")
+
+security_bearer = HTTPBearer()
 
 
 async def get_current_user(
-    token: str = Depends(oauth2_scheme), session: AsyncSession = Depends(get_session)
+    token: HTTPAuthorizationCredentials = Security(security_bearer),
+    session: AsyncSession = Depends(get_session),
 ) -> User:
     try:
         payload = jwt.decode(
-            token, settings.JWT_SECRET_KEY, algorithms=[settings.ALGORITHM]
+            token.credentials, settings.JWT_SECRET_KEY, algorithms=[settings.ALGORITHM]
         )
         token_data = TokenPayload(**payload)
 
